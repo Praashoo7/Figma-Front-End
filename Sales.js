aa0e98fa90b6523fbbf5fb8d30516fdce51522f9
Prashant <rnttl90@gmail.com>
2024-03-04 13:01:53 +0530

    additions

--- Sales.js
+++ Sales.js
@@ -10,3 +10,25 @@ $( function() {
 		,	duration: "fast"
 	});
 } );
+
+
+/*------------------------- CUSTOM-DROPDOWN -------------------------*/
+
+document.addEventListener('DOMContentLoaded', function () {
+	var selectedOption = document.querySelector('.selected_option');
+	var optionsList = document.querySelector('.options');
+  
+	selectedOption.addEventListener('click', function () {
+		optionsList.style.display = optionsList.style.display === 'block' ? 'none' : 'block';
+	});
+  
+	optionsList.addEventListener('click', function (event) {
+		if (event.target.tagName === 'LI') {
+			selectedOption.textContent = event.target.textContent;
+			optionsList.style.display = 'none';
+			// Optionally, you can use the selected value (data-value attribute)
+			var selectedValue = event.target.getAttribute('data_value');
+			console.log('Selected Value:', selectedValue);
+		}
+	});
+  });
