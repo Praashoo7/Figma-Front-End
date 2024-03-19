afc3e86cebc28f360a87081ed00c38124be848cb
Prashant <rnttl90@gmail.com>
2024-03-19 11:59:05 +0530

    Update Login.js

--- JS/Login.js
+++ JS/Login.js
@@ -19,36 +19,30 @@ window.addEventListener("load", function () {
   main_image.style.opacity = 0.2;
 });
 
-
 function redirectToHomePage() {
+
   var BtnText = document.getElementById("login_btn_text");
   var loading = document.getElementById("load");
 
-  // Check if the page is being loaded for the first time or being shown again after navigation
-  if (history && history.state && history.state.reset) {
-      // Reset changes if the page is being shown again after navigation
-      BtnText.style.opacity = 1;
-      loading.style.opacity = 0;
-      document.getElementById("load").style.display = "none";
-      document.getElementById("load").style.opacity = 0;
-      document.getElementById("login_btn_text").style.display = "block";
-      document.getElementById("login_btn_text").style.opacity = 1;
-  } else {
-      BtnText.style.opacity = 0;
-      loading.style.opacity = 1;
+  BtnText.style.opacity = 0;
+  loading.style.opacity = 1;
 
-      document.getElementById("load").style.display = "flex";
-      setTimeout(() => {
-          document.getElementById("load").style.opacity = 1;
-      }, 400);
+  document.getElementById("load").style.display = "flex";
+  setTimeout(() => {
+      document.getElementById("load").style.opacity = 1;
+  }, 400);
 
-      document.getElementById("login_btn_text").style.display = "none";
-      setTimeout(() => {
-          document.getElementById("login_btn_text").style.opacity = 0;
-      }, 400);
+  document.getElementById("login_btn_text").style.display = "none";
+  setTimeout(() => {
+    document.getElementById("login_btn_text").style.opacity = 0;
+  }, 400);
 
-      setTimeout(() => {
-          window.location.href = 'HomePage.html';
-      }, 1500);
-  }
+  setTimeout(() => {
+      window.location.href = 'HomePage.html';
+  }, 1500);
+}
+
+if (!sessionStorage.getItem('pageReloaded')) {
+  sessionStorage.setItem('pageReloaded', 'true');
+  window.location.reload(true);
 }
